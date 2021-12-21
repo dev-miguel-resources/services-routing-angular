4cf1489efc0096265f3ba0d4d380f7d86b1e8e79
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-21 11:12:01 -0300

    finished home, about, navbar and animals components

--- src/app/components/animal-tarjeta/animal-tarjeta.component.ts
+++ src/app/components/animal-tarjeta/animal-tarjeta.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-animal-tarjeta',
   templateUrl: './animal-tarjeta.component.html',
-  styleUrls: ['./animal-tarjeta.component.css']
+  styleUrls: ['./animal-tarjeta.component.css'],
 })
 export class AnimalTarjetaComponent implements OnInit {
+  @Input() animal: any = {};
+  @Input() index: number = 0;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  verAnimal() {
+    this.router.navigate(['/animal', this.index]);
   }
-
 }
--- src/app/components/animales/animales.component.ts
+++ src/app/components/animales/animales.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimalesService, Animal } from '../../services/animales.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-animales',
   templateUrl: './animales.component.html',
-  styleUrls: ['./animales.component.css']
+  styleUrls: ['./animales.component.css'],
 })
 export class AnimalesComponent implements OnInit {
+  animales: Animal[] = [];
 
-  constructor() { }
+  constructor(
+    private _animalesService: AnimalesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.animales = this._animalesService.getAnimales();
   }
 
+  verAnimal(idx: number) {
+    this.router.navigate(['/animal', idx]);
+  }
 }
