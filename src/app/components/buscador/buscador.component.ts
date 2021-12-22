0d15072456be5e8473aa1232aa4e20dc25cc401e
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-22 11:04:07 -0300

    finished project and finished buscador component

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AnimalesService } from '../../services/animales.service';
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
-  styleUrls: ['./buscador.component.css']
+  styleUrls: ['./buscador.component.css'],
 })
 export class BuscadorComponent implements OnInit {
+  animales: any[] = [];
+  termino: string = '';
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _animalesService: AnimalesService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      console.log(params['termino']);
+      this.termino = params['termino'];
+      this.animales = this._animalesService.buscarAnimales(params['termino']);
+      console.log(this.animales);
+    });
   }
-
 }
